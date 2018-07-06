b7aae064f2e063ff171ee7e7dff6a3c06e4ec3f9
Joan Arnaldich <joan.arnaldich@icgc.cat>
2018-07-06 11:45:00 +0200

    afegida consulta exemple

--- gdal_buildindex.sh
+++ gdal_buildindex.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 ./mount_drives.sh
 
-#	-v /mnt/uatd_proces:/dades/datacloud \
-
 docker run -it --rm -v "$PWD/of25c.map:/of25c.map" \
 	-v /mnt/datacloud_2:/dades/datacloud \
 	-v $PWD/dades:/dades:rw \
---
+++ query.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl 'http://172.17.28.152:5000/cgi-bin/mapserv?map=/dades/of25c/of25c.map&SERVICE=WMS&VERSION=1.1.1%20&REQUEST=GetCapabilities'
+
