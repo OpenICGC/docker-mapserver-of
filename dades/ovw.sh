a5eba64dac40b92a3e9ad632fc675391539f5db7
Joan Arnaldich <joan.arnaldich@icgc.cat>
2018-07-09 07:55:55 +0200

    Afegit codi pel COG

--- dades/ovw.sh
+++ dades/ovw.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-#gdalbuildvrt -overwrite /dades/ovw10.vrt -srcnodata 0 -vrtnodata 0 /dades/of25c/tileindex_ovr.shp /dades/datacloud/of25m_ETRS89/tif_unzip/*.tif
+date
+# Uns 20s
+gdalbuildvrt -overwrite /dades/rasters/of5m.vrt -srcnodata 0 -vrtnodata 0 /dades/datacloud/of5m_ETRS89/tif_unzip/*.tif
 #-co COMPRESS=JPEG -co PHOTOMETRIC=YCBCR 
-#gdal_translate -of GTiff -r cubic -tr 10 10 -a_nodata 0 -co "TILED=YES" -co "BIGTIFF=YES" /dades/ovw10.vrt /dades/ovw10.tif
-gdaladdo -r cubic /dades/ovw10.tif 2 4 8 16 32 64 128 256 512 1024 2048 4096
+date
+# Unes 7h
+gdal_translate -of GTiff -r cubic -tr 0.5 0.5 -a_nodata 0 \
+	-co "TILED=YES" -co "BIGTIFF=YES" -co "COMPRESS=DEFLATE" \
+	/dades/rasters/of5m.vrt /dades/rasters/of5m.tif
+date
+gdaladdo -r cubic /dades/rasters/of5m.tif 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192
+date
+gdal_translate /dades/rasters/of5m.tif /dades/rasters/of5m_cog.tif 
+	-co "TILED=YES" -co "BIGTIFF=YES" -co "COPY_SRC_OVERVIEWS=YES" \
+	-co "COMPRESS=JPEG" -co "PHOTOMETRIC=YCBCR" \
+date
+
 
---
+++ gdal_buildovw.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker run -it --rm -v "$PWD/of25c.map:/of25c.map" \
+	-v /mnt/datacloud_2:/dades/datacloud \
+	-v /dades/rasters:/dades/rasters:rw \
+	-v $PWD/dades:/dades:rw \
+	--name gdal2 \
+	geographica/gdal2:2.2.4 \
+	/bin/bash -c /dades/ovw.sh 
