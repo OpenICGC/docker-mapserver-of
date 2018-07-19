0370282901b83f4c5dd071f8556a21dd9e7e0988
Joan Arnaldich <joan.arnaldich@icgc.cat>
2018-07-19 11:04:40 +0200

    First version

--- addos.py
+++ addos.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 import sys
 import os
-
-
+from subprocess import call
 
 #http://betaserver.icgc.cat/geoservice/289213657262513444877732435960631588437?layer=of25cv33sd0f241156s1r070&mode=tile&tilemode=gmap&tile={x}+{y}+{z}
 
 if __name__ == '__main__':
-    fname = sys.argv[1]
+    ovr_fname = fname = sys.argv[1]
+    out_fname = sys.argv[2]
+    levels = int(sys.argv[3])
+    for l in range(levels):
+        call(['gdaladdo', '-ro', '-r', 'gauss', 
+            '--config', 'COMPRESS_OVERVIEW', 'PACKBITS', 
+            '--config', 'GDAL_CACHEMAX', '25%', 
+            '--config', 'BIGTIFF', 'YES', 
+            ovr_fname, '2'])
+        ovr_fname += ".ovw"
+    call(['gdal_translate', fname,  out_fname, 
+          '-co', 'TILED=YES', 
+          '-co', 'BIGTIFF=YES', 
+          '-co', 'COPY_SRC_OVERVIEWS=YES',
+          '-co', 'COMPRESS=DEFLATE'])
+
+
+
+
     print fname
 
