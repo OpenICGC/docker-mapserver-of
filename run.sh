25833de210295a9c59d6d802c02a0be7824601b9
Joan Arnaldich <j.arnaldich@icgc.cat>
2018-11-21 11:47:50 +0100

    Backup

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-./mount_drives.sh
+# ./mount_drives.sh
 #	-v /mnt/uatd_proces:/dades/datacloud \
-docker run -v "$PWD/of25c.map:/of25c.map" -p 5000:5000 \
-	-v /mnt/datacloud_2:/dades/datacloud \
-	-v "$PWD/dades/:/dades/" \
-	-v "/dades/rasters:/rasters/" \
+	# -e DEBUG:3 \
+docker run -d -p 80:5000 \
+	-v "/home/geoproces/docker-mapserver-of/dades/of25c/orto25c.map:/orto25c.map" \
+	-v "/dades/:/dades/" \
 	-v "$PWD/lighttpd.conf:/lighttpd.conf" \
-	-e DEBUG:3 \
 	-v "$PWD/dades/log/:/var/log/" \
 	--cap-add SYS_ADMIN --cap-add DAC_READ_SEARCH \
 	--security-opt seccomp=unconfined --privileged \
-	--name ms --rm \
+        --restart unless-stopped \
+	--name ms \
 	thingswise/mapserver:latest 
