c2bda5adc349bb8c8bbf673993ebb4c22cbbc3a1
Joan Arnaldich <joan.arnaldich@icgc.cat>
2019-06-06 10:36:20 +0200

    Limitat el nombre de processos FCGI

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ docker rm ms_ecw
 docker run -d -p 8080:80 \
 	-v "/home/geoproces/docker-mapserver-of/dades/of25c/orto25c.map:/orto25c.map" \
 	-v "/dades2/:/dades/" \
+	-v "/home/geoproces/docker-mapserver-of/fcgid.conf:/etc/apache2/mods-enabled/fcgid.conf" \
 	-v "/dades2/log/:/var/log/:rw" \
 	-v "/dades2/log/apache2:/var/log/apache2:rw" \
 	-e "MIN_PROCS=3" -e "MAX_PROCS=10" -e "MAX_LOAD_PER_PROC=4" -e "IDLE_TIMEOUT=20" \
